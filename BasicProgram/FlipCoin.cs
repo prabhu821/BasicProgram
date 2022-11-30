071d663573623883a18466baf64a61c7c4bd3c1f
rishiraj007 <rishiraj@mounttalent.com>
2022-11-30 09:43:23 +0530

    [Prabhu] Add: Basic Program Added

---
+++ BasicProgram/FlipCoin.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    public class FlipCoin
+    {
+        int headcount = 0;
+        int tailcount = 0;
+        double headpercentage = 0;
+        double tailpercentage = 0;
+        public void Coin()
+        {
+            Console.WriteLine("Enter The Value For Number of Flips ");
+            int numberofFlip = Convert.ToInt32(Console.ReadLine());
+
+            if (numberofFlip > 0)
+
+            {
+                for (int i = 0; i < numberofFlip; i++)
+                {
+                    Random random = new Random();
+                    int number = random.Next(2);
+
+                    Console.WriteLine(number);
+                    if (number == 0)
+                    {
+                        tailcount = tailcount + 1;
+                    }
+                    else
+                    {
+                        headcount++;
+                    }
+                }
+                headpercentage = headcount + 100 / numberofFlip;
+
+                tailpercentage = tailcount + 100 / numberofFlip;
+
+                Console.WriteLine("Head Percentage " + headpercentage);
+                Console.WriteLine("Tail Percentage " + tailpercentage);
+            }
+            else
+            {
+                Console.WriteLine("The number of Flip is not Valid");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ BasicProgram/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace BasicProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FlipCoin flipCoin = new FlipCoin();
+            flipCoin.Coin();
+            //LeapYears leapYear = new LeapYears();
+            //leapYear.LeapYear();
+            //PowerOf2s powerOf2 = new PowerOf2s();
+            //powerOf2.PowerOf2();
+            //HarmonicSeries harmonic = new HarmonicSeries();
+            //harmonic.Harmonic();
+            //PrimeFactor primeFactor = new PrimeFactor();
+            //primeFactor.Factors();
+            //QuotientAndRemainder quotientAndRemainder = new QuotientAndRemainder();
+            //quotientAndRemainder.Divider();
+            //SwapNumber swapNumber = new SwapNumber();
+            //swapNumber.Swap();
+            //EvenorOdd evenorOdd = new EvenorOdd();
+            //evenorOdd.EvenOdd();
+            //VowelsorConsonants vowelsorConsonants = new VowelsorConsonants();
+            //vowelsorConsonants.VowelorConsonant();
+            //LargestNumber largestNumber = new LargestNumber();
+            //largestNumber.LargeNumber();
+        }
+    }
+}
